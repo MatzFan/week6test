af51810049cb5015d4d84d164c13f0a732a41406
Bruce Steedman <bruce.steedman@gmail.com>
2013-12-28 17:10:16 +0000

    Minor refactor, and spotted bug to fix; if H command given out of range coords, image remains transposed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,15 @@
 require 'stringio'
 
+def capture_output &block
+  old_stdout = $stdout
+  test_stdout = StringIO.new
+  $stdout = test_stdout
+  block.call
+  test_stdout.string
+ensure
+  $stdout = old_stdout
+end
+
 HELLO = "\
 IIIIIIIIIIIIIIIIIIIIIIIIIIII
 IMIIMIMMMMIMIIIIMIIIIIMMIIMI
@@ -11,13 +21,26 @@ IMIIMIMIIIIMIIIIMIIIIMOOMIII
 IMIIMIMMMMIMMMMIMMMMIIMMIIMI
 IIIIIIIIIIIIIIIIIIIIIIIIIIII"
 
-def capture_output &block
-  old_stdout = $stdout
-  test_stdout = StringIO.new
-  $stdout = test_stdout
-  block.call
-  test_stdout.string
-ensure
-  $stdout = old_stdout
+def hello_commands
+  [('I 28 9'),
+   ('V 2 2 8 M'),
+   ('V 5 2 8 M'),
+   ('V 7 2 7 M'),
+   ('V 12 2 7 M'),
+   ('V 17 2 7 M'),
+   ('V 22 3 7 M'),
+   ('V 25 3 7 M'),
+   ('V 27 2 8 M'),
+   ('H 3 10 5 M'),
+   ('H 8 10 2 M'),
+   ('H 7 20 8 M'),
+   ('H 23 24 2 M'),
+   ('H 23 24 8 M'),
+   ('L 6 5 O'),
+   ('L 11 8 O'),
+   ('L 16 8 O'),
+   ('L 27 7 O'),
+   ('F 25 8 I'),
+   ('S')]
 end
 
